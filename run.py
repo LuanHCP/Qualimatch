a08a6bbc18371b6ec1670eb778ecbca2e7198253
Luan H C Pereira <carvalhopereiraluan16@gmail.com>
2026-09-09 13:00:32 -0300

    feat: adiciona base sanitizada da Portfolio Edition

---
+++ run.py
@@ -0,0 +1,11 @@
+import os
+import uvicorn
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "app:app",
+        host=os.getenv("APP_HOST", "127.0.0.1"),
+        port=int(os.getenv("APP_PORT", "8000")),
+        reload=False,
+    )
